afb8ae6424b677ed4a51681f9c6e222aea27d878
LPT <lptalty@gmail.com>
2021-12-26 17:26:15 -0500

    Built out logic for the backend which gets the random numbers from the API and sends them to the client. Basic file structure for the front end. Next steps are to implement game logic with the randomly generated numbers

--- client/App.js
+++ client/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
-
+import NumberService from './services/numberService'
 const App = () => {
+
+  const onClickButton = async (url) => {
+    const result = await NumberService.getComputerNumbers(url);
+    console.log('what is returned to app.js: ', result)
+    console.log(typeof(result))
+    console.log(result[0])
+  }
+  
   return (
     <div>
-      <div> First commit</div>
+      <div> Getting Random Numbers with API</div>
+      <button
+      onClick={(e) =>
+        onClickButton('http://localhost:3000/api/computer')
+      }
+      >
+        Click me
+        </button>
     </div>
   );
 };
---
+++ client/services/numberService.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+class NumberService {
+    static async getComputerNumbers(url) {
+        try {
+            console.log('entered into Number Service, trying to get the number')
+            const result = await axios.get(url);
+            console.log('Here is the result: ', result.data)
+            return result.data;
+        } catch (error) {
+            console.log('There was an error getting computer numbers from services/numberService: ', error);
+            return error;
+        }
+    }
+}
+
+export default NumberService;
