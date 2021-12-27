f945055e736e90a7152ac83e1b243ffd40e64cd9
LPT <lptalty@gmail.com>
2021-12-27 01:06:04 -0500

    Set hooks to update state when clicking the option for zero. Next step is to add that functionality for the rest of the button options, which will save the guess of the user. Once that is finished, will work on the logic for computer to give the user hints for how they did

--- client/App.js
+++ client/App.js
@@ -2,9 +2,11 @@ import React, { component, useEffect, useState } from 'react';
 import NumberService from './services/numberService'
 import {DropdownButton, ButtonGroup, Dropdown} from 'react-bootstrap'
 import styles from './scss/application.scss';
+import { id } from 'postcss-selector-parser';
 
 const App = () => {
   const [numberList, setNumberList] = useState([]);
+  const [guessList, setGuessList] = useState(['First Guess', 'Second Guess', 'Third Guess', 'Fourth Guess'])
 
   const onClickButton = async (url) => {
     const result = await NumberService.getComputerNumbers(url);
@@ -37,20 +39,45 @@ const App = () => {
         <div>{numberList[3]}</div>
         
         <div className='guess-container'>
-            {['First Guess', 'Second Guess', 'Third Guess', 'Fourth Guess'].map(
-        (variant) => (
-          <DropdownButton
-            as={ButtonGroup}
-            key={variant}
-            id={`dropdown-variants-${variant}`}
-            variant={variant.toLowerCase()}
-            title={variant}
-          >
-            <Dropdown.Item eventKey="1">Action</Dropdown.Item>
-            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-          </DropdownButton>
-        ),
-      )}
+            {guessList.map(
+              (variant, index) => (
+                <DropdownButton
+                  as={ButtonGroup}
+                  id={`${index}`}
+                  key={index}
+                  variant={variant.toLowerCase()}
+                  title={variant}
+                >
+                  <Dropdown.Item eventKey="0"
+                   onClick={(e) => {
+                     //have to update the button 1 2 3 4 at index 0 1 2 3 to the selected number using hooks
+                    //how do we know which button we're clicking? 
+                    console.log('Index should be 3: ', index)
+                    console.log('value should be 0', e.target.innerText)
+                    console.log(guessList)
+                    guessList[index] = e.target.innerText;
+                    console.log(guessList)
+                    setGuessList([...guessList]);
+
+                    //this works
+                    // setGuestList(['please']);   
+                   }
+                  }
+                  >0</Dropdown.Item>
+
+
+                  <Dropdown.Item eventKey="1"
+                  onClick={(e) => console.log(e)}
+                  >1</Dropdown.Item>
+                  <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                  <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                  <Dropdown.Item eventKey="4">4</Dropdown.Item>
+                  <Dropdown.Item eventKey="5">5</Dropdown.Item>
+                  <Dropdown.Item eventKey="6">6</Dropdown.Item>
+                  <Dropdown.Item eventKey="7">7</Dropdown.Item>
+                </DropdownButton>
+              ),
+            )}
         </div>
     </div>
   );
